c65a9a8da830bdd717938d3255df5cd951e5e60f
commandstring <bmxmikey2@gmail.com>
2023-04-28 17:19:28 -0400

    Renamed delete to destroy for consistency

--- src/Controller.php
+++ src/Controller.php
@@ -5,6 +5,6 @@ namespace Tnapf\SessionInterfaces;
 interface Controller {
     public function get(string $id): ?Session;
     public function create(): Session;
-    public function delete(Session $session): void;
+    public function destroy(Session $session): void;
     public function save(Session $session): void;
 }

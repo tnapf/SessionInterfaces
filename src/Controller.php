edec3f52d0249d091a6e326387461fa82e579d9c
commandstring <bmxmikey2@gmail.com>
2023-04-28 17:16:08 -0400

    Removed comments

--- src/Controller.php
+++ src/Controller.php
@@ -2,29 +2,9 @@
 
 namespace Tnapf\SessionInterfaces;
 
-use Tnapf\SessionInterfaces\Exceptions\SessionDoesNotExist;
-
 interface Controller {
-    /**
-     * @return Session
-     */
+    public function get(string $id): ?Session;
     public function create(): Session;
-
-    /**
-     * @param string $id
-     * 
-     * @throws SessionDoesNotExist **MUST** be thrown if there's no session associated with the provided id or if the session is expired
-     */
-    public function get(string $id): Session;
-
-    /**
-     * @param Session $session
-     * @return void
-     */
     public function delete(Session $session): void;
-
-    /**
-     * You **MUST** store/update sessions grabbed
-     */
-    public function __destruct();
+    public function save(Session $session): void;
 }
